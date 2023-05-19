28f2b0c332b05fca43e4d13da24c36ba98c31735
hunghg255 <giahunghust@gmail.com>
2023-05-19 13:18:29 +0700

    update

--- src/layout/components/MainHeader/index.tsx
+++ src/layout/components/MainHeader/index.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Header = () => {
   return (
     <>
-      <h1>Hello Header</h1>
+      <h1 className='rounded bg-blue-500    py-2 px-4 text-base text-white'>Hello Header</h1>
     </>
   );
 };
