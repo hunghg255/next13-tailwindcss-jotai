4890a04ce7693df0513b9d8977733ab730927faa
hunghg255 <giahunghust@gmail.com>
2023-06-01 16:29:49 +0700

    update lint

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,13 @@
 import '../styles/tailwind.css';
 import '../styles/globals.scss';
 
-import type { ReactElement, ReactNode } from 'react';
+import { ReactElement, ReactNode } from 'react';
 
 import { Waterfall } from '@next/font/google';
 import type { NextPage } from 'next';
-import { appWithTranslation } from 'next-i18next';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
+import { appWithTranslation } from 'next-i18next';
 
 import ErrorBoundary from '@components/ErrorBoundary';
 import AppLayout from '@layout/AppLayout';
@@ -17,7 +17,6 @@ import nextI18nConfig from '../next-i18next.config';
 export type NextPageWithLayout = NextPage & {
   getLayout?: (page: ReactElement) => ReactNode;
 };
-
 type AppPropsWithLayout = AppProps & {
   Component: NextPageWithLayout;
 };
@@ -34,16 +33,16 @@ function MyApp({ Component, pageProps }: AppPropsWithLayout) {
   return (
     <>
       <Head>
-        <meta name='robots' content='index, follow' />
-        <meta name='googlebot' content={'index,follow'} />
-        <meta charSet='utf-8' />
-        <meta name='theme-color' content='#476055' />
-        <meta name='title' content='Maby Client' />
-        <meta name='description' content='Maby Client' />
-        <link rel='shortcut icon' href='/static/favicon.ico' />
+        <meta name="robots" content="index, follow" />
+        <meta name="googlebot" content={'index,follow'} />
+        <meta charSet="utf-8" />
+        <meta name="theme-color" content="#476055" />
+        <meta name="title" content="Maby Client" />
+        <meta name="description" content="Maby Client" />
+        <link rel="shortcut icon" href="/static/favicon.ico" />
         <meta
-          name='viewport'
-          content='width=device-width,initial-scale=1,maximum-scale=2,shrink-to-fit=no'
+          name="viewport"
+          content="width=device-width,initial-scale=1,maximum-scale=2,shrink-to-fit=no"
         />
       </Head>
 
