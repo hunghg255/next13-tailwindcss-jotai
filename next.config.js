cc119c3a107f84e512a20c7985577da0ecb8977b
hunghg255 <giahunghust@gmail.com>
2023-12-27 20:57:20 +0700

    chore: update pkg

--- next.config.js
+++ next.config.js
@@ -1,32 +1,50 @@
+const { withHydrationOverlay } = require('next-hydration-overlay/next');
+
 const { i18n } = require('./next-i18next.config');
 
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
 
+const isProd = process.env.NODE_ENV === 'production';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // i18n
   i18n,
   swcMinify: true,
+  output: 'standalone',
 
   // config env
   publicRuntimeConfig: {
     NODE_ENV: process.env.NODE_ENV,
     APP_API_URL: process.env.NEXT_PUBLIC_APP_API_URL,
+    LOCAL_STORAGE_KEY: process.env.NEXT_PUBLIC_LOCAL_STORAGE_KEY,
   },
   images: {
     minimumCacheTTL: 60,
-    domains: [],
+    formats: ['image/webp'],
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: '**',
+      },
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
   },
   httpAgentOptions: {
     keepAlive: false,
   },
   headers: async function headers() {
-    if (process.env.NODE_ENV === 'development') return [];
+    if (process.env.NODE_ENV === 'development') {
+      return [];
+    }
     return [
       {
-        source: '/:all*(svg|jpg|jpeg|png|webp|avif|otf|ttf)',
+        source: '/:all*(svg|jpg|jpeg|png|webp|avif|otf|ttf|woff|woff2|eot)',
         locale: false,
         headers: [
           {
@@ -39,4 +57,4 @@ const nextConfig = {
   },
 };
 
-module.exports = withBundleAnalyzer(nextConfig);
+module.exports = isProd ? nextConfig : withHydrationOverlay()(withBundleAnalyzer(nextConfig));
